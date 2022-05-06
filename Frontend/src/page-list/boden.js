2a4fe62eb405058ae13ffeab65f6efa516b12e47
hasanhero99 <marcelheld99@gmx.de>
2022-05-06 13:36:54 +0200

    Anpassung der Wettkampfbewertung

--- Frontend/src/page-list/boden.js
+++ Frontend/src/page-list/boden.js
@@ -33,24 +33,24 @@ export default class Boden extends Page {
     // Hier findet die berechnung bei Klicken auf den Button statt
     async berechne(){
         let templateElement = this._mainElement.querySelector(".list");
-        var ergebnis;
+        var ergebnis = 0;
+        
         for(var i = 1; i < 3; i++){
-            this.team = [];
             ergebnis = 0;
+            this.team = [];
             for(var j = 1; j < 5; j++){
                 this.hilfAusgang = templateElement.querySelector(".inp_auteam" + i + j).value;
                 this.hilfAbzug = templateElement.querySelector(".inp_abteam" + i + j).value;
                 this.ergebnis = 10 + parseFloat(this.hilfAusgang) - parseFloat(this.hilfAbzug);
-                console.log(Math.max.apply(Math,this.team));
                 templateElement.querySelector(".text_ergebnis" + i + j).value = this.ergebnis.toFixed(2);
-
+                this.team[j - 1] = this.ergebnis;
             }
             var myIndex = this.team.indexOf(Math.min.apply(Math,this.team));
             if (myIndex !== -1) {
-            this.team.splice(myIndex, 1);
+                this.team.splice(myIndex, 1);
             }
-            for(let i = 0; i < this.team.length; i++){
-                ergebnis += this.team[i];
+            for(let k = 0; k < this.team.length; k++){
+                ergebnis += this.team[k];
             }
             templateElement.querySelector(".text_ergebnis_gesamt" + i).value = ergebnis.toFixed(2);
             
